73d5e3c00afccaaf3e2bdc3394cbdb55532291d0
Guest User <Guest@Insights-Mac-mini-2.local>
2019-08-26 11:53:19 -0300

    Fazendo requisicao de filtro

--- Server.py
+++ Server.py
@@ -1,10 +1,18 @@
 from flask import Flask, render_template, request, redirect, jsonify, make_response
+from os import listdir
+from os.path import isfile, join
 from werkzeug.utils import secure_filename
 import json
-from flask_cors import CORS
+#from flask_cors import CORS
+'''
+REGRAS DA APLICAÇÃO:
+
+Para cada efeito haverá duas pastas: Antes e depois. Assim, teremos
+o controle sobre o do e undo. 
+'''
 
 app = Flask(__name__)
-CORS(app)  
+#CORS(app)  
 
 if __name__ == '__main__':    
     app.run(host='127.0.0.1', port=5000, debug=True)
@@ -18,5 +26,49 @@ def upload_image():
 	
 	if request.method == 'POST':
 		f = request.files['file']
-		f.save('static/images/' + secure_filename(f.content_type).replace('_','.'))
+		f.save('static/images/original/' + secure_filename(f.content_type).replace('_','.'))
 	return json.dumps({'success':True}), 200, {'ContentType':'application/json'} 
+
+# Aqui será a parte de gestao de filtros aplicados.
+# A ideia é receber apenas uma string e aqui decidir o que aplicar.
+@app.route('/apply_filter', methods=['POST'])
+def apply_filter():
+	
+	# Pegando nome do arquivo
+	# path = "static/images/original/"
+	# onlyfiles = listdir(path)
+	# file = onlyfiles[0]
+	# print(file)
+
+	if request.method == 'POST':
+		filter_ = request.form['filter']
+		if filter_ == 'negative':
+			print(filter_)
+		#f.save('static/images/original/' + secure_filename(f.content_type).replace('_','.'))
+	return json.dumps({'success':True}), 200, {'ContentType':'application/json'} 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ static/scripts/script.js
@@ -0,0 +1,53 @@
+$(function() { //shorthand document.ready function
+    
+    // Para pegar as alterações no arquivo a ser submetidas
+	var form;
+	$('#file').change(function (event) {
+    	form = new FormData();
+    	form.append('file', event.target.files[0]); 
+	});
+
+	$("#submit").click(function() {
+        $.ajax({
+        	type: "POST",
+        	url: "/upload_image",
+        	data: form,
+        	processData: false,
+            	contentType: false,
+        	success: function() {
+    	      	//display message back to user here
+    	      	document.getElementById('imageid').src = "static/images/original/image.png?" + new Date().getTime();
+        	},
+        	error: function (request, status, erro) {
+                alert("Problema ocorrido: " + status + "\nDescição: " + erro);
+                //Abaixo está listando os header do conteudo que você requisitou, só para confirmar se você setou os header e dataType corretos
+                alert("Informações da requisição: \n" + request.getAllResponseHeaders());
+            }
+        });
+        return false;
+	});
+
+    // Para aplicar filtros. O controle é feito com o id do botão.
+    // Todo filtro terá um botão com o nome do filtro por ele aplicado. 
+    $(".filter").click(function() {
+        var formFilter = new FormData();
+        formFilter.append('filter', this.id); 
+
+        $.ajax({
+            type: "POST",
+            url: "/apply_filter",
+            data: formFilter,
+            processData: false,
+                contentType: false,
+            success: function() {
+                //display message back to user here
+                document.getElementById('imageid').src = "static/images/original/image.png?" + new Date().getTime();
+            },
+            error: function (request, status, erro) {
+                alert("Problema ocorrido: " + status + "\nDescição: " + erro);
+                //Abaixo está listando os header do conteudo que você requisitou, só para confirmar se você setou os header e dataType corretos
+                alert("Informações da requisição: \n" + request.getAllResponseHeaders());
+            }
+        });
+    });
+});
