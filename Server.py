4ab64e6a9684db28c85508ee11da3997a70c531e
Dayana Rocha <dayanarochadev@gmail.com>
2019-09-12 16:08:00 -0300

    Iniciando alterações do front

--- Server.py
+++ Server.py
@@ -22,8 +22,8 @@ Instalacoes pro image_processing:
 app = Flask(__name__)
 path_original = "static/images/original/"
 path_actual = "static/images/actual/"
-filters_list = ["negative", "log", "power", "histogram"]
-non_filters_list = ["non-negative", "non-log", "non-power", "non-histogram"]
+filters_list = ["negative", "log", "power", "histogram", "convolution", "mean", "median"]
+non_filters_list = ["non-negative", "non-log", "non-power", "non-histogram", "non-convolution", "non-mean", "non-median"]
 global filters_in_use 
 filters_in_use = []
 #CORS(app)  
--- utils.py
+++ utils.py
@@ -9,7 +9,7 @@ def cleaningFolder(directory):
 		remove(directory + file)
 
 # Aplicar filtro
-def applyFilter(filter_, img_matrix):
+def applyFilter(filter_, img_matrix, extra=None):
 
 	if filter_ == 'negative':
 		img_matrix = pi.filterNegative(img_matrix)
@@ -25,6 +25,18 @@ def applyFilter(filter_, img_matrix):
 
 	elif filter_ == 'histogram':
 		img_matrix = pi.filterHistogram(img_matrix)
+
+	elif filter_ == 'convolution':
+		# Aqui o extra é uma matriz que é um filtro
+		img_matrix = pi.filterConvolution(img_matrix, extra)
+
+	elif filter_ == 'mean':
+		# Aqui o extra é um n, que é a dimensão da máscara da matriz
+		img_matrix = pi.filterMean(img_matrix, extra)
+
+	elif filter_ == 'median':
+		# Aqui o extra é um n, que é a dimensão da máscara da matriz
+		img_matrix = pi.filterMedian(img_matrix, extra)
 
 	return img_matrix
 
